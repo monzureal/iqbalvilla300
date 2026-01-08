b4af233bdd13d998252007b47fdc153d28d950b8
monzureal <monzurul247@gmail.com>
2026-01-08 22:13:00 +0600

    Update auth.js

--- admin-panel-310/auth.js
+++ admin-panel-310/auth.js
@@ -1,11 +1,17 @@
 function checkPassword() {
-  const PASSWORD = "amiroshu@68662"; // change this
+  const PASSWORD = "IqbalVilla310"; // change this
   const entered = document.getElementById("password").value;
 
   if (entered === PASSWORD) {
     sessionStorage.setItem("admin", "true");
-    window.location.href = "index.html";
+
+    // Redirect on correct password (FILL PATH IF NEEDED)
+    window.location.href = "/login.html";
+
   } else {
-    alert("Wrong password");
+    alert("Wrong password!");
+
+    // Redirect on wrong password (YOU will fill this path)
+    window.location.href = "/reject.html";
   }
 }
