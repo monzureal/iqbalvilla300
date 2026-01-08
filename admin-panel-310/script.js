8ffd6163dd0fd02d5448c2fb8bc7323b0767c3f8
monzureal <monzurul247@gmail.com>
2026-01-08 21:34:18 +0600

    Create script.js

---
+++ admin-panel-310/script.js
@@ -0,0 +1,13 @@
+(function () {
+  const PASSWORD = "IqbalVilla310"; // CHANGE THIS
+
+  const entered = prompt("Enter access password:");
+
+  if (entered === PASSWORD) {
+    document.body.style.display = "block";
+  } else {
+    alert("Access denied!");
+    document.body.innerHTML = "";
+    window.location.href = "../wifi/index.html";
+  }
+})();
