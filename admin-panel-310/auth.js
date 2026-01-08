24b758aea91e6bf910e78eb76e17f98cf99c3332
monzureal <monzurul247@gmail.com>
2026-01-08 21:44:07 +0600

    Update auth.js

--- admin-panel-310/auth.js
+++ admin-panel-310/auth.js
@@ -1,1 +1,11 @@
+function checkPassword() {
+  const PASSWORD = "IqbalVilla310"; // change this
+  const entered = document.getElementById("password").value;
 
+  if (entered === PASSWORD) {
+    // redirect to the REAL input page
+    window.location.href = "index.html";
+  } else {
+    alert("Wrong password!");
+  }
+}
